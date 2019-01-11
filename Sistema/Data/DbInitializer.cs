9181d7b27b9a2ae81623ce0e1618b42618e9b99a
ErnestoVM <ernestovm07@gmail.com>
2019-01-11 15:31:47 -0600

    Initial commit

---
+++ Sistema/Data/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sistema.Models;
+
+namespace Sistema.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SistemaContext context)
+        {
+            context.Database.EnsureCreated();
+
+            //buscar si exsten registros en la tabla categoria
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+
+            var categorias = new Categoria[]
+            {
+                new Categoria{ Nombre="Programacion", Descripcion="Cursos de Programacion", Estado = true},
+                new Categoria{ Nombre="Diseño Grafico", Descripcion="Cursos de diseño Grafico", Estado = true}
+            };
+
+            foreach(Categoria c in categorias)
+            {
+                context.Categoria.Add(c);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
